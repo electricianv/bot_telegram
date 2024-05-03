b06a3d495f1f5c99585f5ca480f6f9e7e3acef81
Vasil <electricianv@gmail.com>
2024-05-03 13:47:20 +0100

    added missed files

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '7081381962:AAECU89g0T1SOvnoT1Iya8UV2Vj_4KmVxbE'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+    }
